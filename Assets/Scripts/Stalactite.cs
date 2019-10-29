65d51ea5445322c4f51ceba3fbecfe91a31a4c00
emmalizz <elisowski@icloud.com>
2019-10-29 12:41:00 -0700

    Stalactite falls when above the player

--- Assets/Scripts/Stalactite.cs
+++ Assets/Scripts/Stalactite.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Stalactite : MonoBehaviour
 {
+    private float time;
+    public GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,8 @@ public class Stalactite : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        if (Player.transform.position.x == this.transform.position.x) this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic; //stalactite falls when player is directly under it
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
