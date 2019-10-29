f6ebce39bbd68fca8814d90547394431744233e4
emmalizz <elisowski@icloud.com>
2019-10-28 20:55:52 -0700

    Initial stalactite implementation

---
+++ Assets/Scripts/Stalactite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stalactite : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            print("ouuuuuch!"); //replace with health decrement when health implemented
+        }
+    }
+}
