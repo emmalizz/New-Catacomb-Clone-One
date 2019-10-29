3c7a2bc15627504f25e0d1f51d2123c56177ad97
MagicTurtle <raffertychen@gmail.com>
2019-10-29 13:24:40 -0700

    Basic Movement
    Move with the left and right keys (or a and d), basic jump that doesn't care if the player is on the ground or not (so the player can just play indefinitely).

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D obj;
+
+    public float speed;
+    public float jumpHeight;
+
+    private float distToGround;
+
+    void Start()
+    {
+        obj = GetComponent<Rigidbody2D>();        
+    }
+
+    void Update()
+    {
+        Vector2 velocity = new Vector2(Input.GetAxis("Horizontal"), 0) ;
+        
+        if (Input.GetButton("Jump"))
+        {
+            velocity.y = jumpHeight;
+        }
+
+        obj.MovePosition(obj.position + (velocity * speed * Time.deltaTime));
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        // Debug-draw all contact points and normals
+        foreach (ContactPoint contact in collisionInfo.contacts)
+        {
+            Debug.Log("Collision");
+        }
+    }
+}
