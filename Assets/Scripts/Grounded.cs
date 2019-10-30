e5cdc01a9f33fbe57c5beb75e611674c52d63701
MagicTurtle <raffertychen@gmail.com>
2019-10-30 04:22:34 -0700

    Jumping (not smooth)
    The player can now jump only if they are jumping on the ground, but the movement isn't smooth compared to what I've seen in other people's works even though I followed exactly what they did

---
+++ Assets/Scripts/Grounded.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    GameObject Player;
+
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("On ground");
+        Player.GetComponent<PlayerMovement>().isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("Left ground");
+        Player.GetComponent<PlayerMovement>().isGrounded = false;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,34 +6,25 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D obj;
 
+    public bool isGrounded = false;
+
     public float speed;
     public float jumpHeight;
 
-    private float distToGround;
-
     void Start()
     {
-        obj = GetComponent<Rigidbody2D>();        
+        obj = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        Vector2 velocity = new Vector2(Input.GetAxis("Horizontal"), 0) ;
-        
-        if (Input.GetButton("Jump"))
-        {
-            velocity.y = jumpHeight;
-        }
+        Vector2 velocity = new Vector2(Input.GetAxis("Horizontal"), 0f);
 
         obj.MovePosition(obj.position + (velocity * speed * Time.deltaTime));
-    }
 
-    void OnCollisionStay(Collision collisionInfo)
-    {
-        // Debug-draw all contact points and normals
-        foreach (ContactPoint contact in collisionInfo.contacts)
+        if (Input.GetButton("Jump") && isGrounded)
         {
-            Debug.Log("Collision");
+            obj.AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
         }
     }
 }
