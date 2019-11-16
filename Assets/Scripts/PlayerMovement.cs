fa60b0278031cf7c5ff6bdfa797ccd248f218295
MagicTurtle <raffertychen@gmail.com>
2019-11-15 16:24:18 -0800

    Add dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,15 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed;
     public float jumpHeight;
+    public float dashSpeed;
+
+    private int dashCooldown = 0;
+
+    private bool doJump;
+    private bool doDash;
+
+    private float facing = 1;
+    private float movementDirection;
 
     void Start()
     {
@@ -18,13 +27,51 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        Vector2 velocity = new Vector2(Input.GetAxis("Horizontal"), 0f);
+        movementDirection = Input.GetAxis("Horizontal");
+
+        if (movementDirection > 0)
+        {
+            facing = 1;
+        }
+        else if (movementDirection < 0)
+        {
+            facing = -1;
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            doJump = true;
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            doDash = true;
+        }
+
+        if (dashCooldown > 0)
+        {
+            dashCooldown--;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = new Vector2(movementDirection, 0f);
 
         obj.MovePosition(obj.position + (velocity * speed * Time.deltaTime));
 
-        if (Input.GetButton("Jump") && isGrounded)
+        if (doJump)
         {
-            obj.AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+            if (isGrounded)
+            {
+                obj.AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+            }
+            doJump = false;
+        }
+        if (doDash)
+        {
+            obj.AddForce(new Vector2(dashSpeed * facing, 0f), ForceMode2D.Impulse);
+            doDash = false;
         }
     }
 }
