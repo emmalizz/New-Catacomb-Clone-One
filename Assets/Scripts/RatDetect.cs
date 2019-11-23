cac5ded320194fa53ac400bed402bce8c0e555b3
MagicTurtle <raffertychen@gmail.com>
2019-11-22 16:14:24 -0800

    Added Rats and RoidRats

---
+++ Assets/Scripts/RatDetect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatDetect : MonoBehaviour
+{
+    GameObject Rat;
+
+    void Start()
+    {
+        Rat = gameObject.transform.parent.gameObject;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Top collided");
+        Destroy(Rat);
+    }
+}
---
+++ Assets/Scripts/RatMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatMove : MonoBehaviour
+{
+    public Rigidbody2D obj;
+
+    public int moveDistance;
+    public int speed;
+    
+    private int facing = 1;
+    private int movement = 0;
+    void Start()
+    {
+        obj = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movement != moveDistance)
+        {
+            Vector2 velocity = new Vector2(facing, 0f);
+            obj.MovePosition(obj.position + (velocity * speed * Time.deltaTime));
+            movement++;
+        }
+        else
+        {
+            facing *= -1;
+            movement = 0;
+        }
+    }
+}
